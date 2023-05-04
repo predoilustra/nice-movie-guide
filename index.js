08be229193b85e1e8c6873f6a767b868dafffb42
Pedro Augusto <131421771+predoilustra@users.noreply.github.com>
2023-05-04 00:46:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,18 @@ let movieNameRef = document.getElementById("movie-name");
 let searchBtn = document.getElementById("search-btn");
 let result = document.getElementById("result")
 
-//function to fetch data from api
-
 let getMovie = () => {
     let movieName = movieNameRef.value;
     let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
     
-    //if input field is empty
-
     if(movieName.length <= 0){
         result.innerHTML = `<h3 class="msg">Please enter a movie name </h3>`;
 
     }
 
-    //if input isn't empty
     else{
         fetch(url).then((resp) => resp.json()).then((data) =>{
-            //if movie exist in database
+   
             if(data.Response == "True"){
                 result.innerHTML = `
                     <div class="info">
@@ -46,13 +41,12 @@ let getMovie = () => {
                 `;
             }
 
-            //if movie doesn't exist in database
             else{
                 result.innerHTML = `<h3 class="msg">${data.Error}</
                 h3>`;
             }
         })
-        //if error occurs
+        
         .catch(() =>{
             result.innerHTML = `<h3 class"msg">Error Occured</h3>`;
         });
